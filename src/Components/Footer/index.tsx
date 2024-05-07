821ea0e49280806586553afde93cda82085044d3
Mak Trnka <maktrnka@comcast.net>
2024-05-07 10:09:01 -0700

    Update and rename index.jsx to index.tsx

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer: React.FunctionComponent = () => {
+    return <footer>
+      &copy; 2018
+    </footer>;
+};
+
+export default Footer;
+
+//index.jsx
+// import React from 'react';
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+// export default Footer;
