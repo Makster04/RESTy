c588e680ed8c88eb6cf96161eabc331da1f883c9
Mak Trnka <maktrnka@comcast.net>
2024-05-07 10:21:25 -0700

    Update index.tsx

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,37 +1,49 @@
 import React from 'react';
 
-import './Form.scss';
+const Header: React.FunctionComponent = () => {
+      return <header>
+    <h1>RESTy</h1>
+  </header>;
+  };
 
-class Form extends React.Component {
+export default Header;
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
-    this.props.handleApiCall(formData);
-  }
+// index.jsx
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
-  }
-}
+// import React from 'react';
 
-export default Form;
+// import './Form.scss';
+
+// class Form extends React.Component {
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const formData = {
+//       method:'GET',
+//       url: 'https://pokeapi.co/api/v2/pokemon',
+//     };
+//     this.props.handleApiCall(formData);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit}>
+//           <label >
+//             <span>URL: </span>
+//             <input name='url' type='text' />
+//             <button type="submit">GO!</button>
+//           </label>
+//           <label className="methods">
+//             <span id="get">GET</span>
+//             <span id="post">POST</span>
+//             <span id="put">PUT</span>
+//             <span id="delete">DELETE</span>
+//           </label>
+//         </form>
+//       </>
+//     );
+//   }
+// }
+
+// export default Form;
